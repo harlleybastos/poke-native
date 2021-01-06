0b66c7604c60a84c3386ace735e75ef00fc3e19a
Harlley Bastos <harlleybastos@hotmail.com>
2021-01-06 15:16:17 -0300

    Starting to add SearchBar

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,8 @@
 import React, {useEffect, useState} from 'react';
-import {FlatList, View, StatusBar} from 'react-native';
+import {FlatList, View, StatusBar, Button, Text} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 
-import {TextInput, TouchableOpacity} from 'react-native-gesture-handler';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 import PokeCard from '../../components/PokeCard';
 import {PokeImageHeader} from '../../components/PokeImage/styles';
 import {usePoke} from '../../context/PokeDate';
@@ -10,6 +10,7 @@ import {TextInputSearch} from './styles';
 function Main() {
   const {pokemon} = usePoke();
   const nav = useNavigation();
+  const [inputValue, setInputValue] = useState('');
 
   const [filteredPokemonList, setFilteredPokemonList] = useState([]);
   const [offset, setOffset] = useState(20);
@@ -17,6 +18,15 @@ function Main() {
   useEffect(() => {
     setFilteredPokemonList(pokemon.slice(0, offset));
   }, [offset, pokemon]);
+
+  const handleSearch = () => {
+    if (inputValue) {
+      const filteredListPok = pokemon.filter((pokemon) => {
+        return pokemon.name.includes(inputValue);
+      });
+      setFilteredPokemonList(filteredListPok);
+    }
+  };
 
   const loadMore = () => {
     setOffset((oldOffset) => oldOffset + 20);
@@ -28,9 +38,33 @@ function Main() {
         resizeMode={'contain'}
         source={require('../../image/pokeLogo.png')}
       />
-      <View style={{alignItems: 'center', bottom: 90}}>
-        <TextInputSearch placeholder={'Search a pokemon...'} />
-      </View>
+      {/* <View style={{alignItems: 'center', bottom: 90}}>
+        <TextInputSearch
+          onChangeText={(e) => setInputValue(e.target.value)}
+          value={inputValue}
+          placeholder={'Search a pokemon...'}
+        />
+        <TouchableOpacity
+          onPress={() => handleSearch}
+          style={{
+            top: 15,
+            padding: 10,
+            height: 50,
+            width: 200,
+            backgroundColor: 'rgba(10, 50, 0, 0.2)',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <Text
+            style={{
+              fontFamily: 'Roboto-Medium',
+              textTransform: 'uppercase',
+              fontSize: 25,
+            }}>
+            Search
+          </Text>
+        </TouchableOpacity>
+      </View> */}
       <FlatList
         style={{bottom: 60}}
         numColumns={2}
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -3,7 +3,7 @@ const TextInputSearch = styled.TextInput`
   font-size: 30px;
   font-family: 'Roboto-Medium';
   text-decoration: none;
-  width: 350px;
+  width: 320px;
   padding: 25px;
   border: 1px solid rgba(0, 0, 0, 0.3);
 `;
